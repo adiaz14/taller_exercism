553b831600f20c5e89da24e9a601954ef46ee023
Andres Diaz <adiaz14@live.com>
2021-05-25 23:38:35 -0500

    commit matrix

---
+++ matrix/matrix.js
@@ -0,0 +1,37 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor(matrix) {
+    this.matrix = matrix;
+  }
+
+  get columns() {
+    const columns = [];
+    
+    for (let i = 0; i <= this.rows.length; i++) {
+      columns[i] = this.rows.map((row) => {
+        return row[i];
+      });
+    }
+    return columns;
+  }
+
+  get rows() {
+    let rows = this.matrix;
+
+    rows = rows.split("\n");
+    const resultado = rows.map((row) => {
+      const dividir_resultado = row.split(" ");
+
+      return dividir_resultado.map((number) => {
+        return Number(number);
+      });
+    });
+    return resultado;
+  }
+
+
+}
